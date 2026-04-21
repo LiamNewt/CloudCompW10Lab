f245e28d7554cd55997f5c4301dc46f1552d1c64
LiamNewt <S00186320@atu.ie>
2026-04-21 02:13:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API = "https://471axqtup3.execute-api.eu-west-1.amazonaws.com/upload-url";
+const API = "https://471axqtup3.execute-api.eu-west-1.amazonaws.com";
 
 async function uploadFile() {
     const file = document.getElementById("fileInput").files[0];
